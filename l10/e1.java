efbc4b3a8e90d235dfe3170f333614473d680c8a
silkslime <silkslime@yandex.ru>
2023-04-14 16:53:29 +0300

    добавил файлики для лаб

---
+++ l10/e1.java
@@ -0,0 +1,11 @@
+package l10;
+
+public class e1 {
+    /**
+     * XXXXX
+     * XXXXX
+     */
+    public static void main(String[] args) {
+               
+    }
+}
---
+++ l9/e4.java
@@ -0,0 +1,19 @@
+package l9;
+
+public class e4 {
+    /**
+     * Вариант 2. Задача 2.
+     * Использовать ТОЛЬКО методы Stream API. Циклов и условий быть не должно.
+     * Задана коллекция: 
+     * (Класс People: имя и возраст)
+     * Collection<People> peoples = Arrays.asList(
+     *         new People("Ivan", 16),
+     *         new People("Petr", 23),
+     *         new People("Maria", 42)
+     * );
+     * Отсортировать по имени в обратном алфавитном порядке. 
+     */
+    public static void main(String[] args) {
+               
+    }
+}
